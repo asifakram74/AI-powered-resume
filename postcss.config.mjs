4cc9ed62271d69d7815114b1486fb349339c5d2c
asifakram <asifakram74@gmail.com>
2025-09-04 18:18:58 +0500

    update

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+export default {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss: {}, // ✅ CORRECT
+    autoprefixer: {},
   },
 }
-
-export default config
