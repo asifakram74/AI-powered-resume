d6d75d60f2ae60148ef1353f156bca97982ae1ab
asifakram <asifakram74@gmail.com>
2025-09-10 22:40:18 +0500

    upddate

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -95,6 +95,7 @@ export default function SignInPage() {
     localStorage.setItem("google_oauth_state", randomState);
 
     const redirectUri = window.location.origin + window.location.pathname;
+    console.log("redirectUri",redirectUri)
     window.location.href = `https://backendcv.onlinetoolpot.com/api/auth/google/redirect?state=${randomState}&redirect_uri=${encodeURIComponent(redirectUri)}`;
     // window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/auth/google/redirect?state=${randomState}&redirect_uri=${encodeURIComponent(redirectUri)}`;
   };
