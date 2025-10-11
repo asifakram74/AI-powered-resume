670b557720282448e653b126a28d1fbe13927ba0
Kamran xheikh <kamrancrossmediasole@gmail.com>
2025-10-11 17:35:14 +0500

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,8 @@ import { Toaster } from "sonner";
 import HydrateAuth from "../components/auth/HydrateAuth";
 
 export const metadata: Metadata = {
-  title: "Resumaic – AI-Powered Resume Builder Online",
-  description:
-    "Build professional resumes in minutes with Resumaic, the AI-powered resume builder. Create tailored CVs that stand out and boost your career success.",
+ title: "Resumaic – AI-Powered Resume Builder Online",
+  description: "Build professional resumes in minutes with Resumaic, the AI-powered resume builder. Create tailored CVs that stand out and boost your career success.",
 
   icons: {
     icon: "/favicon.png",
