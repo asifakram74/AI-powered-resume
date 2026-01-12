e7cd424ad4caba7bd4aabbe660aaca4442206a6e
asifakram <asifakram74@gmail.com>
2026-01-13 00:07:37 +0500

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,11 @@
-import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import "../app/globals.css";
 import { ReduxProvider } from "../lib/redux/provider";
 import { Toaster } from "sonner";
 import HydrateAuth from "../components/auth/HydrateAuth";
 import Head from "next/head";
-export default function App({ Component, pageProps }: AppProps) {
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      import("@microsoft/clarity").then((clarity) => {
-        clarity.start({ projectId: "v0az88rsoo" });
-      });
-    }
-  }, []);
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
