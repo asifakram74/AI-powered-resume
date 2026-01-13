62238d45d90b6489ccd537ffc3a5a2c48c5ec043
asifakram <asifakram74@gmail.com>
2026-01-14 01:24:18 +0500

    Revert "Merge branch 'production-build'"
    This reverts commit 5fce3b62aa49a3f976fa020c7ee4f23417a67bcc, reversing
    changes made to 76feac81749e89db660da54c717e192cbeafc33a.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,11 @@ import "./globals.css";
 import { ReduxProvider } from "../lib/redux/provider";
 import { Toaster } from "sonner";
 import HydrateAuth from "../components/auth/HydrateAuth";
-import Script from "next/script";
+import { ThemeProvider } from "../components/theme-provider";
+import { ThemeToggle } from "../components/theme-toggle";
 
 export const metadata: Metadata = {
-  title: "Resumaic – AI-Powered Resume Builder Online",
+ title: "Resumaic – AI-Powered Resume Builder Online",
   description: "Build professional resumes in minutes with Resumaic, the AI-powered resume builder. Create tailored CVs that stand out and boost your career success.",
 
   icons: {
@@ -59,28 +60,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="antialiased">
+    <html lang="en" className="antialiased" suppressHydrationWarning>
       <body className="font-sans">
-
-        {/* Tawk.to */}
-        <Script id="tawk" strategy="afterInteractive">
-          {`
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/6964eeec46ee65197d4f8c96/1jep4ajkp';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-            })();
-          `}
-        </Script>
-        <ReduxProvider>
-          <HydrateAuth />
-          <Toaster position="top-right" richColors />
-          {children}
-        </ReduxProvider>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
+          <ReduxProvider>
+            <HydrateAuth />
+            <Toaster position="top-right" richColors />
+            <ThemeToggle />
+            {children}
+          </ReduxProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
