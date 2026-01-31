63dfa9ca6645ae0eb2c5cd3c3fbb5bb99c15cc16
asifakram <asifakram74@gmail.com>
2026-01-31 18:38:54 +0500

    update

--- lib/redux/service/pasonaService.ts
+++ lib/redux/service/pasonaService.ts
@@ -62,11 +62,7 @@ const constructProfilePictureUrl = (profilePicture: string | null | undefined): 
     return profilePicture;
   }
   if (profilePicture.startsWith('media/')) {
-<<<<<<< Updated upstream
     return `https://stagingbackend.resumaic.com/storage/app/public/${profilePicture}`;
-=======
-    return `https://stagingbackend.resumaic.com/api/public /storage/app/public/${profilePicture}`;
->>>>>>> Stashed changes
   }
   return profilePicture;
 };
